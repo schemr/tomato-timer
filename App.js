59f0016e5da504c30df005a3cd86c37416a45991
schemr <schemryj@gmail.com>
2017-12-17 17:06:46 +0900

    Add create store, provider App.js

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import Timer from './components/Timer';
 
+import reducer from './reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+let store = createStore(reducer);
+
 export default class App extends Component {
   render() {
-    return <Timer />;
+    return (
+      <Provider store={store}>
+        <Timer />
+      </Provider>
+    )
   }
 }
